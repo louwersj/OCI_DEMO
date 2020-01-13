4b638fd0f84e8f3ad2b28db524db41d67d3cd583
Johan Louwers <louwersj@gmail.com>
2020-01-13 10:38:15 +0100

    Update .tf file
    Added a more usable version of the .tf file to the example.

--- deployment/environment/terraform/oci_deployment.tf
+++ deployment/environment/terraform/oci_deployment.tf
@@ -1,3 +1,17 @@
-provider "oci" {
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "region" {}
+variable "private_key_path" {}
+variable "private_key_password" {}
 
+
+
+provider "oci" {
+  tenancy_ocid = "${var.tenancy_ocid}"
+  user_ocid = "${var.user_ocid}"
+  fingerprint = "${var.fingerprint}"
+  region = "${var.region}"
+  private_key_path = "${var.private_key_path}"
+  private_key_password = "${var.private_key_password}"
 }

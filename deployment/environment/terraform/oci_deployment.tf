f7934905c28122ec3187959d372aa4bdd3176ac6
Johan Louwers <louwersj@gmail.com>
2020-01-17 12:20:41 +0100

    added resources
    Adding VCN, gateway, default route table

--- deployment/environment/terraform/oci_deployment.tf
+++ deployment/environment/terraform/oci_deployment.tf
@@ -8,7 +8,8 @@ variable "private_key_password" {}
 variable "compartment_ocid" {}
 variable "source_version" {}
 
-#define the OCI provider.
+
+# Define the OCI provider.
 provider "oci" {
   tenancy_ocid = "$var.tenancy_ocid"
   user_ocid = "$var.user_ocid"
@@ -18,12 +19,13 @@ provider "oci" {
   private_key_password = "$var.private_key_password"
 }
 
+
 # Create the OCI compartment for the deployment. This compartment will be 
 # placed under a root compartment (not per definition the tenancy root.
 resource "oci_identity_compartment" "deployment_compartment" {
     compartment_id = "${var.compartment_ocid}"
     description = "Automatic test compartment for build: ${var.source_version}"
-    name = "compartment_${var.source_version}"
+    name = "build_${var.source_version}"
     freeform_tags =  {
            application_environment = "test"
            application_name = "OCI_DEMO"
@@ -32,11 +34,56 @@ resource "oci_identity_compartment" "deployment_compartment" {
    }
 }
 
+
+# Create the virtual cloud network for the deployment. This compartment will 
+# be associated with the newly created "deployment_compartment"
+#
+# TODO : we have to find a way to find a free CIDR block as multiple build
+#        could be active (deployed) at the same time. We need a dynamic way
+# .      of deciding which CIDR block could be used.
 resource "oci_core_vcn" "deployment_vcn" {
   cidr_block     = "10.1.1.0/16"
   dns_label      = "${var.source_version}"
   compartment_id = "${oci_identity_compartment.deployment_compartment.id}"
-  display_name   = "${var.source_version}"
+  display_name   = "build_${var.source_version}"
+  freeform_tags =  {
+         application_environment = "test"
+         application_name = "OCI_DEMO"
+         git_build = "${var.source_version}"
+         git_branch = "master"
+   }
+}
+
+
+# Create an internet gateway for the deployment. This internet gateway will 
+# be associated with the newly created "deployment_compartment" and the newly
+# created VCN "deployment_vcn"
+resource "oci_core_internet_gateway" "deployment_internet_gateway" {
+  compartment_id = "${oci_identity_compartment.deployment_compartment.id}"
+  display_name   = "build_${var.source_version}"
+  vcn_id         = "${oci_core_vcn.deployment_vcn.id}"
+  freeform_tags =  {
+         application_environment = "test"
+         application_name = "OCI_DEMO"
+         git_build = "${var.source_version}"
+         git_branch = "master"
+   }
+}
+
+
+# Create a default route table. Do note that you can have multiple route tables.
+# this defaultroute table will route to the deployment_internet_gateway.
+#
+# TODO : check on manage_default_resource_id = "${oci_core_vcn.vcn1.default_route_table_id}"
+resource "oci_core_default_route_table" "default_route_table" {
+  compartment_id = "${oci_identity_compartment.deployment_compartment.id}"
+  vcn_id         = "${oci_core_vcn.deployment_vcn.id}"  
+  display_name   = "defaultRT_${var.source_version}"
+  route_rules {
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = "${oci_core_internet_gateway.deployment_internet_gateway.id}"
+  }
   freeform_tags =  {
          application_environment = "test"
          application_name = "OCI_DEMO"

64a54085fd039641d42eb727e34a6c56cbaeebd8
Johan Louwers <louwersj@gmail.com>
2020-01-29 08:56:13 +0100

    Update oci_deployment.tf

--- deployment/environment/terraform/oci_deployment.tf
+++ deployment/environment/terraform/oci_deployment.tf
@@ -17,7 +17,7 @@ provider "oci" {
   tenancy_ocid = "$var.tenancy_ocid"
   user_ocid = "$var.user_ocid"
   fingerprint = "$var.fingerprint"
-  region = "$var.region"
+  region = "${var.region}"
   private_key_path = "$var.private_key_path"
   private_key_password = "$var.private_key_password"
 }
